ae03cbb859f8e40827892bfec472443b1ea327b4
Samuel Buis <samuel.buis@inra.fr>
2023-03-08 13:14:17 -0500

    Included the ability to read observations from different experiments

--- R/test_read_obs.R
+++ R/test_read_obs.R
@@ -1,13 +1,15 @@
 library(DSSAT)
 library(dplyr)
 library(CroptimizR)
-source("read_obs.R")
+source("R/read_obs.R")
 
 model_options <- vector("list")
-model_options$DSSAT_path <- 'C:\\DSSAT47'
-model_options$DSSAT_exe <-  'DSCSM047.EXE'
+model_options$DSSAT_path <- 'C:\\DSSAT48'
+model_options$DSSAT_exe <-  'DSCSM048.EXE'
 model_options$Crop <- "Wheat"
-model_options$experiment_file <- "SWSW7501.WHX"
+model_options$ecotype_filename <- "WHCER048.ECO"
 
-obs_list <- read_obs(model_options)
+situation_name<- c("AQTB1101_2", "AQTB1101_10", "AQTB1201_5", "AQTB1201_15") 
+
+obs_list <- read_obs(model_options, situation_name)
 CroptimizR:::is.obs(obs_list)
